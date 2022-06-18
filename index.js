9095d1785e3bd3aea9599c08a5288cc5295a485c
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-18 11:46:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
-/* Your Code Here */
+function createEmployeeRecord ([firstName, familyName , title , payPerHour]) { 
+    let empObj = {
+        firstName : firstName,
+        familyName : familyName,
+        title : title,
+        payPerHour : payPerHour,
+        timeInEvents : [],
+        timeOutEvents: [],
+    }
+    return empObj
+}
+function createEmployeeRecords (recordsArray){
+    return recordsArray.map(createEmployeeRecord)
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+const createTimeInEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
+const createTimeOutEvent = function (datestamp){
+    let [date, hour] = datestamp.split(' ');
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour),
+        date: date
+    });
+    return this  
+};
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+const hoursWorkedOnDate = function (date){
+    const inEvent = this.timeInEvents.find(inEvent=>inEvent.date === date);
+    const outEvent = this.timeOutEvents.find(outEvent=>outEvent.date === date);
+            return ((outEvent.hour - inEvent.hour)/100)
+};
+
+const wagesEarnedOnDate = function (date){
+    return hoursWorkedOnDate.apply(this, [date])* this.payPerHour
+};
+
+
+
+
+function calculatePayroll(recordsArray){
+   let payroll = recordsArray.reduce((acc, cur)=> {
+       return acc + allWagesFor.call(cur)
+   }, 0)
+   return payroll
+    }
+
+function findEmployeeByFirstName(srcArray, firstName){
+    let emp = srcArray.find((rec) => rec.firstName === firstName)
+    return emp;
+}
+
+
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
